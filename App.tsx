84f25cd5dfd953e18c03a9ce29c8dd04f465615f
ItsRau1 <00001085823507sp@al.educacao.sp.gov.br>
2023-02-21 19:51:24 -0300

    fix estructure | init home screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, } from 'react-native';
-import { Testando } from './page/testando';
+import { Home } from "./src/screens/home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Testando />
-    </View>
+    <Home />
   );
 }
-
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
