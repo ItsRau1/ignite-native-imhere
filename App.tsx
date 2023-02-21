2b37029a49c9080ee5d8c3ec376678cf012370fb
ItsRau1 <00001085823507sp@al.educacao.sp.gov.br>
2023-02-21 16:54:05 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
+import { Testando } from './page/testando';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Testando />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
---
+++ page/testando.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar"
+import { Text, View, InputModeOptions } from "react-native"
+
+
+export function Testando() {
+    return (
+        <>
+            <Text>Hello World(Ola Pessuilll)</Text>
+            <StatusBar style="auto" />
+        </>
+    )
+}
